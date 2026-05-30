d1b479f96d2c251c0c8b270be6ab0b02d31f0d59
CyberGhost07s <cyberghost.sgd@gmail.com>
2026-05-30 18:00:07 +0700

    Update test.py

--- prototype/test.py
+++ prototype/test.py
@@ -1,1 +1,7 @@
 print("CyberLens Prototype Started")
+print("=== CyberLens Navigation Prototype ===")
+
+destination = input("Enter destination: ")
+
+print(f"Navigating to {destination}...")
+print("Turn left in 50 meters")
